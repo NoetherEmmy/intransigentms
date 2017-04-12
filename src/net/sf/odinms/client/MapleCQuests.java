2611cd463e45955fcd56c1544a97d09712bc7533
Emmy Noether <intransigentms@gmail.com>
2017-04-11 18:54:29 -0700

    lol

--- src/net/sf/odinms/client/MapleCQuests.java
+++ src/net/sf/odinms/client/MapleCQuests.java
@@ -219,7 +219,7 @@ public class MapleCQuests {
             // Data is stored using Unix line endings (LF) because that's our locale,
             // but the client expects Windows-style (CRLF), so we replace the line
             // endings here since clients will have to display `info`.
-            q.info = t.getString("info").replaceAll("\\n\\n", "\r\n\r\n").replaceAll("([^\\r])\\n", "$1\r\n");
+            q.info = t.getString("info").replaceAll("\n", "\r\n");
             final Boolean repeatable = t.getBoolean("repeatable");
             if (repeatable != null) q.repeatable = repeatable;
 
